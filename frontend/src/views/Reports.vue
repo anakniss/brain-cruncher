eb4dd6049aef24fe9e242b31d989abdc6d4e9075
ErickLB <123411342+ErickLemosB@users.noreply.github.com>
2024-12-02 00:42:22 -0300

    Update Reports.vue
    Removing Console.log and comments

--- frontend/src/views/Reports.vue
+++ frontend/src/views/Reports.vue
@@ -19,7 +19,6 @@ interface Report {
   completedQuizCount: number;
 }
 
-// Modal state
 const isModalOpen = ref(false);
 const selectedExamResults = ref<ExamResults[]>([]);
 
@@ -230,7 +229,6 @@ const setAllUserReports = async () => {
       </div>
     </div>
 
-    <!-- Modal -->
     <div v-if="isModalOpen" class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
       <div class="bg-white p-6 rounded-lg shadow-lg max-w-4xl w-full max-h-[80vh] overflow-y-auto">
         <div class="flex justify-between items-center mb-4">
