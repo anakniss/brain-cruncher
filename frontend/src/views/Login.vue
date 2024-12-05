090cd8795594cff2375ed7642149adcee6a3e340
Melissa-Moreira-Oliveira <143560534+Melissa-Moreira-Oliveira@users.noreply.github.com>
2024-12-05 00:14:12 -0300

    Update Login.vue

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -9,7 +9,8 @@ const router = useRouter();
 const email = ref('');
 const password = ref('');
 const error = ref('');
-const passwordError = ref<string | null>(null); 
+const passwordError = ref<string | null>(null);
+const showPassword = ref(false);
 
 const validatePassword = () => {
   const senha = password.value;
@@ -41,46 +42,59 @@ const handleLogin = async () => {
 </script>
 
 <template>
-  <div class="max-w-md mx-auto">
-    <div class="content-box">
-      <h2 class="text-2xl font-bold text-black mb-6">Login</h2>
-
-      <form @submit.prevent="handleLogin" class="space-y-4">
-        <div>
-          <label for="email" class="block text-sm text-black font-medium">Email</label>
-          <input
-            id="email"
-            v-model="email"
-            type="email"
-            required
-            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 text-gray-900"
-          />
-        </div>
-
-        <div>
-          <label for="password" class="block text-sm text-black font-medium">Password</label>
-          <input
-            id="password"
-            v-model="password"
-            @input="validatePassword"
-            type="password"
-            required
-            class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 text-gray-900"
-          />
-          <span v-if="passwordError" class="text-red-600 text-sm">
-            {{ passwordError }}
-          </span>
-        </div>
-
-        <div v-if="error" class="text-red-300 text-sm">{{ error }}</div>
-
-        <button
-          type="submit"
-          class="w-full flex text-black justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium bg-[#a68ba8] hover:bg-[#815a83] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          Login
-        </button>
-      </form>
+    <div class="max-w-2xl mx-auto">
+      <div class="bg-[#d3e1e4] rounded-lg shadow p-6">
+        <h2 class="text-2xl font-bold text-black mb-6">Login</h2>
+  
+        <form @submit.prevent="handleLogin" class="space-y-4">
+          <div>
+            <label for="email" class="block text-sm text-black font-medium">Email</label>
+            <input
+              id="email"
+              v-model="email"
+              type="email"
+              required
+              class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 text-gray-900"
+            />
+          </div>
+  
+          <div>
+            <label for="password" class="block text-sm text-black font-medium">Password</label>
+            <div class="relative">
+              <input
+                id="password"
+                v-model="password"
+                @input="validatePassword"
+                :type="showPassword ? 'text' : 'password'"
+                required
+                class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 text-gray-900 pr-10"
+              />
+              <button
+                type="button"
+                @click="showPassword = !showPassword"
+                class="absolute inset-y-0 right-0 pr-3 flex items-center"
+              >
+                <img 
+                  :src="showPassword ? '/src/assets/show.png' : '/src/assets/hide.png'" 
+                  alt="toggle password visibility"
+                  class="w-5 h-5"
+                />
+              </button>
+            </div>
+            <span v-if="passwordError" class="text-red-600 text-sm">
+              {{ passwordError }}
+            </span>
+          </div>
+  
+          <div v-if="error" class="text-red-300 text-sm">{{ error }}</div>
+  
+          <button
+            type="submit"
+            class="w-full flex text-white justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium bg-[#256C7B] hover:bg-[#194b56] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Login
+          </button>
+        </form>
+      </div>
     </div>
-  </div>
 </template>
